8248610e39a4bd8c19020c25d1dde147f64f9d65
Julian Ciccioli <julian.ciccioli@ecloudsolutions.com>
2026-03-19 13:53:23 -0300

    fix(types): default button as button element and widen VPin items type

--- src/types/button.ts
+++ src/types/button.ts
@@ -6,18 +6,18 @@ export type ButtonPropsBasics = {
 // Choose between button and a elements.
 export type Button = ButtonPropsBasics &
   (
-    | (JSX.IntrinsicElements["a"] & { as?: "a" })
-    | (JSX.IntrinsicElements["button"] & { as: "button" })
+    | (JSX.IntrinsicElements["button"] & { as?: "button" })
+    | (JSX.IntrinsicElements["a"] & { as: "a" })
   );
 
 export type MagneticButton = ButtonPropsBasics & {
   // If you want to use another button. Otherwise it will use the Button component.
   button?: React.ReactElement;
-  // To determine how much it will move. Default: 30.
+  // To determine how much it will move. Default: 0.5.
   movement?: number;
 } & (
-    | (JSX.IntrinsicElements["a"] & { as?: "a" })
-    | (JSX.IntrinsicElements["button"] & { as: "button" })
+    | (JSX.IntrinsicElements["button"] & { as?: "button" })
+    | (JSX.IntrinsicElements["a"] & { as: "a" })
   );
 
 export type ElasticButton = ButtonPropsBasics & {
@@ -26,6 +26,6 @@ export type ElasticButton = ButtonPropsBasics & {
   // Custom html usually with an icon. Default: <div class="spark-elastic__icon"></div>.
   icon?: React.ReactElement;
 } & (
-    | (JSX.IntrinsicElements["a"] & { as?: "a" })
-    | (JSX.IntrinsicElements["button"] & { as: "button" })
+    | (JSX.IntrinsicElements["button"] & { as?: "button" })
+    | (JSX.IntrinsicElements["a"] & { as: "a" })
   );
